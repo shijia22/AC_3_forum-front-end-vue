042e949da5ea1a6111e59469eca720b8a6c2b84e
shichia22 <shichia0207@gmail.com>
2021-08-24 21:08:47 +0800

    feat: 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,7 @@ const routes = [
   {
     path: '/', // 網址字串
     name: 'Home', // 可以省略，某種情況下可以來指派路由
-    component: Home // Vue 元件名稱
+    component: Home, // Vue 元件名稱
   },
   {
     path: '/about',
@@ -16,8 +17,14 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '*', // 星號 (*) 是萬用字元，代表所有網址
+    name: 'not-found',
+    component: NotFound,
+  },
 ]
 
 const router = new VueRouter({
