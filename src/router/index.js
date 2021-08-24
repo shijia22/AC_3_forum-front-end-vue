8c084a89f399bd6d843568cde981e2e5daada1b4
shichia22 <shichia0207@gmail.com>
2021-08-24 22:11:22 +0800

    feat: remove some code & assets

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/', // 網址字串
-    name: 'Home', // 可以省略，某種情況下可以來指派路由
-    component: Home, // Vue 元件名稱
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
   {
     path: '*', // 星號 (*) 是萬用字元，代表所有網址
     name: 'not-found',
