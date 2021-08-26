deb7d00a92d661d41f446105eaef0a38880d5126
shichia22 <shichia0207@gmail.com>
2021-08-26 15:54:06 +0800

    feat: index route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import SignIn from '../views/SignIn.vue'
 import NotFound from '../views/NotFound.vue'
+import SignIn from '../views/SignIn.vue'
+import Restaurants from '../views/Restaurants.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/restaurants',
+  },
   {
     path: '/signin',
     name: 'sign-in',
@@ -15,6 +21,26 @@ const routes = [
     path: '/signup',
     name: 'sign-up',
     component: () => import('../views/SignUp.vue'),
+  },
+  {
+    path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurants,
+  },
+  {
+    path: '/restaurants/feeds',
+    name: 'restaurants-feeds',
+    component: () => import('../views/RestaurantsFeeds.vue'),
+  },
+  {
+    path: '/restaurants/top',
+    name: 'restaurants-tops',
+    component: () => import('../views/RestaurantsTop.vue'),
+  },
+  {
+    path: '/users/top',
+    name: 'users-top',
+    component: () => import('../views/UsersTop.vue'),
   },
   {
     path: '*', // 星號 (*) 是萬用字元，代表所有網址
