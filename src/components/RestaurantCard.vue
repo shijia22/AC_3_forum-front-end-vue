38325d602ebf0737fc5052fb5ecf9d97977de6dc
shichia22 <shichia0207@gmail.com>
2021-08-27 19:32:36 +0800

    feat: addFavorite & Liked fuction

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -7,41 +7,46 @@
         alt="Card image cap"
         width="286px"
         height="180px"
-      >
+      />
       <div class="card-body">
         <p class="card-text title-wrap">
           <a href="#">
             {{ restaurant.name }}
           </a>
         </p>
-        <span class="badge badge-secondary">{{ restaurant.Category ? restaurant.Category.name : '未分類' }}</span>
+        <span class="badge badge-secondary">{{
+          restaurant.Category ? restaurant.Category.name : '未分類'
+        }}</span>
         <p class="card-text text-truncate">
           {{ restaurant.description }}
         </p>
       </div>
       <div class="card-footer">
         <button
+          v-if="restaurant.isFavorited"
+          @click.stop.prevent="deleteFavorite"
           type="button"
           class="btn btn-danger btn-border favorite mr-2"
         >
           移除最愛
         </button>
         <button
+          v-else
+          @click.stop.prevent="addFavorite"
           type="button"
           class="btn btn-primary btn-border favorite mr-2"
         >
           加到最愛
         </button>
         <button
+          v-if="restaurant.isLiked"
+           @click.stop.prevent="deleteLike"
           type="button"
           class="btn btn-danger like mr-2"
         >
           Unlike
         </button>
-        <button
-          type="button"
-          class="btn btn-primary like mr-2"
-        >
+        <button v-else @click.stop.prevent="addLike" type="button" class="btn btn-primary like mr-2">
           Like
         </button>
       </div>
@@ -54,13 +59,39 @@ export default {
   props: {
     initialRestaurant: {
       type: Object,
-      required: true
+      required: true,
+    },
+  },
+  data() {
+    return {
+      restaurant: this.initialRestaurant,
     }
   },
-  data () {
-    return {
-      restaurant: this.initialRestaurant
+  methods: {
+    addFavorite() {
+      this.restaurant = {
+        ...this.restaurant,
+        isFavorited: true,
+      }
+    },
+    deleteFavorite() {
+      this.restaurant = {
+        ...this.restaurant,
+        isFavorited: false,
+      }
+    },
+        addLike () {
+      this.restaurant = {
+        ...this.restaurant,
+        isLiked: true
+      }
+    },
+    deleteLike () {
+      this.restaurant = {
+        ...this.restaurant,
+        isLiked: false
+      }
     }
-  }
+  },
 }
 </script>
