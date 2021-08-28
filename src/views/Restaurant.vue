643a9d3e2da71b625f0f094cc04f7b3699bc1d5c
shichia22 <shichia0207@gmail.com>
2021-08-28 20:09:56 +0800

    feat: RestaurantDetail

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -2,6 +2,7 @@
   <div class="container py-5">
     <h1>餐廳描述頁</h1>
     <!-- 餐廳資訊頁 RestaurantDetail -->
+    <RestaurantDetail :initial-restaurant="restaurant"/>
     <hr />
     <!-- 餐廳評論 RestaurantComments -->
     <!-- 新增評論 CreateComment -->
@@ -9,6 +10,8 @@
 </template>
 
 <script>
+import RestaurantDetail from './../components/RestaurantDetail.vue'
+
 const dummyData = {
   restaurant: {
     id: 1,
@@ -56,6 +59,9 @@ const dummyData = {
 }
 
 export default {
+  components: {
+    RestaurantDetail,
+  },
   data() {
     return {
       restaurant: {
