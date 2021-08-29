38ccde476932be5187436ffcbf2b998d56f22d53
shichia22 <shichia0207@gmail.com>
2021-08-29 23:21:45 +0800

    feat: 後台新增餐廳：製作表單(除 image)

--- src/views/AdminRestaurants.vue
+++ src/views/AdminRestaurants.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="container py-5">
     <!-- 後台導覽頁籤 AdminNav -->
-<AdminNav />
-    <a
-      href="#"
-      class="btn btn-primary mb-4"
-    >
+    <AdminNav />
+    <router-link to="/admin/restaurants/new" class="btn btn-primary mb-4">
       New Restaurant
-    </a>
+    </router-link>
     <!-- 後台餐廳列表 AdminRestaurantsTable -->
     <AdminRestaurantsTable />
   </div>
@@ -21,6 +18,6 @@ export default {
   components: {
     AdminNav,
     AdminRestaurantsTable,
-  }
+  },
 }
 </script>
