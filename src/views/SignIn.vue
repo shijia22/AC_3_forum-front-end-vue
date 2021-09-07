0e9f836014ce2a15d032fbc5813413b25fe3ab95
shichia22 <shichia0207@gmail.com>
2021-09-07 19:25:52 +0800

    feat: SignIn - 發送登入請求

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -56,6 +56,8 @@
 </template>
 
 <script>
+import authorizationAPI from './../apis/authorization'
+
 export default {
   data () {
     return {
@@ -65,13 +67,17 @@ export default {
   },
   methods: {
     handleSubmit () {
-      const data = JSON.stringify({
+     authorizationAPI.signIn({
         email: this.email,
-        password: this.password,
+        password: this.password
+      }).then(response => {
+        // 取得 API 請求後的資料
+        const { data } = response
+        // 將 token 存放在 localStorage 內
+        localStorage.setItem('token', data.token)
+         // 成功登入後轉址到餐廳首頁
+        this.$router.push('/restaurants')
       })
-
-      // TODO: 向後端驗證使用者登入資訊是否合法
-      console.log('data', data)
     },
   },
 }
