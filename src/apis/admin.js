b57f6e08f7318833d7f5c59ad892c5709abbf341
shichia22 <shichia0207@gmail.com>
2021-09-10 22:49:42 +0800

    feat: 後台餐廳刪除功能 API

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -22,5 +22,12 @@ export default {
         headers: { Authorization: `Bearer ${getToken()}` },
       })
     },
+    delete({ restaurantId }) {
+      return apiHelper.delete(`/admin/restaurants/${restaurantId}`, {
+        headers: {
+          Authorization: `Bearer ${getToken()}`,
+        },
+      })
+    },
   },
 }
