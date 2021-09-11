62e28b043fee154c1502991c67fed7bd44d98eba
shichia22 <shichia0207@gmail.com>
2021-09-11 20:54:25 +0800

    feat: AdminRestaurantEdit 送出 PUT 請求

--- src/views/AdminRestaurantEdit.vue
+++ src/views/AdminRestaurantEdit.vue
@@ -3,6 +3,7 @@
     <!-- 餐廳表單 AdminRestaurantForm -->
     <AdminRestaurantForm
       @after-submit="handleAfterSubmit"
+      :is-processing="isProcessing"
       :initial-restaurant="restaurant"
     />
   </div>
@@ -12,19 +13,6 @@
 import AdminRestaurantForm from './../components/AdminRestaurantForm.vue'
 import adminAPI from './../apis/admin'
 import { Toast } from './../utils/helpers'
-const dummyData = {
-  restaurant: {
-    id: 1,
-    name: 'Laurence Reynolds',
-    tel: '1-657-067-3756 x9782',
-    address: '187 Kirlin Squares',
-    opening_hours: '08:00',
-    description: 'sit est mollitia',
-    image:
-      'https://loremflickr.com/320/240/restaurant,food/?random=91.29816290184887',
-    CategoryId: 3,
-  },
-}
 
 export default {
   name: 'AdminRestaurantEdit',
@@ -44,6 +32,7 @@ export default {
         image: '',
         categoryId: '',
       },
+      isProcessing: false
     }
   },
   created() {
@@ -51,10 +40,25 @@ export default {
     this.fetchRestaurant(id)
   },
   methods: {
-    handleAfterSubmit(formData) {
-      // 透過 API 將表單資料送到伺服器
-      for (let [name, value] of formData.entries()) {
-        console.log(name + ': ' + value)
+    async handleAfterSubmit(formData) {
+          try {
+        this.isProcessing = true
+        const { data } = await adminAPI.restaurants.update({
+          restaurantId: this.restaurant.id,
+          formData
+        })
+
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+
+        this.$router.push({ name: 'admin-restaurants' })
+      } catch (error) {
+        this.isProcessing = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法更新餐廳資料，請稍後再試'
+        })
       }
     },
     async fetchRestaurant(restaurantId) {
