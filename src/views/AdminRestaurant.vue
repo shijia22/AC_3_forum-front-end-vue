f853c767260acc08d944b380fb01f2c1fe8616c8
shichia22 <shichia0207@gmail.com>
2021-09-11 21:11:13 +0800

    feat: AdminRestaurant API

--- src/views/AdminRestaurant.vue
+++ src/views/AdminRestaurant.vue
@@ -43,33 +43,13 @@
 
 <script>
 import { emptyImageFilter } from './../utils/mixins'
-
-const dummyData = {
-  restaurant: {
-    id: 2,
-    name: 'Mrs. Mckenzie Johnston',
-    tel: '567-714-6131 x621',
-    address: '61371 Rosalinda Knoll',
-    opening_hours: '08:00',
-    description:
-      'Quia pariatur perferendis architecto tenetur omnis pariatur tempore.',
-    image: 'https://loremflickr.com/320/240/food,dessert,restaurant/?random=2',
-    createdAt: '2019-06-22T09:00:43.000Z',
-    updatedAt: '2019-06-22T09:00:43.000Z',
-    CategoryId: 3,
-    Category: {
-      id: 3,
-      name: '義大利料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    }
-  }
-}
+import adminAPI from './../apis/admin'
+import { Toast } from './../utils/helpers'
 
 export default {
   name: 'AdminRestaurant',
   mixins: [emptyImageFilter],
-  data () {
+  data() {
     return {
       restaurant: {
         id: -1,
@@ -79,41 +59,61 @@ export default {
         openingHours: '',
         tel: '',
         address: '',
-        description: ''
-      }
+        description: '',
+      },
     }
   },
-  mounted () {
+  mounted() {
     const { id: restaurantId } = this.$route.params
     this.fetchRestaurant(restaurantId)
   },
-  methods: {
-    fetchRestaurant() { //restaurantId
-      const { restaurant } = dummyData
-      const {
-        id,
-        name,
-        Category,
-        image,
-        opening_hours: openingHours,
-        tel,
-        address,
-        description,
-      } = restaurant
-
+  beforeRouteUpdate(to, from, next) {
+    // 路由改變時重新抓取資料
+    const { id } = to.params
+    this.fetchRestaurant(id)
+    next()
+  },
+  watch: {
+    initialRestaurant(newValue) {
       this.restaurant = {
-        id,
-        name,
-        categoryName: Category ? Category.name : '未分類',
-        image,
-        openingHours,
-        tel,
-        address,
-        description,
+        ...this.restaurant,
+        ...newValue,
       }
-
-      this.restaurantComments = dummyData.restaurant.Comments
     },
-  }
+  },
+  methods: {
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data } = await adminAPI.restaurants.getDetail({ restaurantId })
+
+        const {
+          id,
+          name,
+          Category,
+          image,
+          opening_hours: openingHours,
+          tel,
+          address,
+          description,
+        } = data.restaurant
+
+        this.restaurant = {
+          id,
+          name,
+          categoryName: Category ? Category.name : '未分類',
+          image,
+          openingHours,
+          tel,
+          address,
+          description,
+        }
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試',
+        })
+      }
+    },
+  },
 }
 </script>
