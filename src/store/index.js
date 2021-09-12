8de5ea42712c6886130fb4b746524806ab3e953f
shichia22 <shichia0207@gmail.com>
2021-09-12 17:05:50 +0800

    feat: 設定 currentUser 和 isAuthenticated

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,30 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // data，每個 Vue 元件都可以使用
   state: {
+    currentUser: {
+      id: -1,
+      name: '',
+      email: '',
+      image: '',
+      isAdmin: false,
+    },
+    isAuthenticated: false,
   },
+  // 用來修改 state 的資料
   mutations: {
+    setCurrentUser(state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        // 將 API 取得的 currentUser 覆蓋掉 Vuex state 中的 currentUser
+        ...currentUser,
+      }
+      // 將使用者的登入狀態改為 true
+      state.isAuthenticated = true
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
+  // 設定其他的非同步函式，例如發送 API 請求
+  actions: {},
+  modules: {},
 })
